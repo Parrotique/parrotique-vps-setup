3f9e6d2b0c59d3e3d001e9f0a476477d27988387
efimpolianskii <82118384+efimpolianskii@users.noreply.github.com>
2024-08-11 18:46:32 +0400

    Update docker-installation.sh

--- docker-installation.sh
+++ docker-installation.sh
@@ -1,4 +1,4 @@
-echo -e "\e[32m[*] Installing docker and docker-compose\e[0m"
+echo -e "\e[32m[*] Docker and docker-compose installation and set up\e[0m"
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
@@ -6,3 +6,5 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
 sudo apt install docker-compose
+
+echo -e "\e[32m[*] Docker and docker-compose installation and set up was completed\e[0m"
